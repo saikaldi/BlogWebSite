9c19b74f5e287662fc77e4c94df037fc50dd3142
saikaldi <saikalulukbek24@gmail.com>
2026-01-17 09:43:21 +0100

    Add enabled Detaield button

--- src/main/java/com/example/commercialApp/models/Post.java
+++ src/main/java/com/example/commercialApp/models/Post.java
@@ -13,6 +13,16 @@ public class Post {
     private String title, anons, full_text;
     private int views;
 
+    public Post(String title, String anons, String full_text){
+        this.title=title;
+        this.anons=anons;
+        this.full_text=full_text;
+    }
+
+    public Post(){
+
+    }
+
     public String getFull_text() {
         return full_text;
     }
